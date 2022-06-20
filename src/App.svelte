79f141a852f2093d8eadaf52f7910e024ae65143
OAALLTECHINFO <102165261+OAALLTECHINFO@users.noreply.github.com>
2022-06-20 14:47:24 +0400

    Add files via upload

---
+++ src/App.svelte
@@ -0,0 +1,18 @@
+
+<script>
+	import { TextBox,TextBlock,ToggleSwitch } from "fluent-svelte";
+</script>
+
+<h1>The OA</h1>
+<TextBox placeholder="TextBox" />
+<ToggleSwitch checked />
+
+<style>
+    @import url("https://unpkg.com/fluent-svelte/theme.css");
+
+    /* Some base styles to get things looking right. */
+    :global(body) {
+        background-color: var(--fds-solid-background-base);
+        color: var(--fds-text-primary);
+    }
+</style>
